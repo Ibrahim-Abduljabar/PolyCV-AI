a06457b06bf5d543b77d94f849d0c7d3e8fcd0b4
ابراهيم عبد الجبار خان احمد خان عيسى <himo4007@gmail.com>
2026-06-25 13:56:35 +0300

    app.py

--- app.py
+++ app.py
@@ -197,9 +197,7 @@ if st.button("🚀 Process and Inject Data Into Premium Template", use_container
                                 )
                                 
                                 raw_json = completion.choices.message.content
-                                clean_json = clean_json_string(raw_json)
-                                
-                                try:
-                                    cv_data = json.loads(clean_json)
+                                clean_json = clean_json_string(raw_json)      
+                                cv_data = json.loads(clean_json)
                                     
 
